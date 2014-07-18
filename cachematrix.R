416540e8e115dc05c0366d542f78b07eb0f023d4
jblewitt <blewitt.j@gmail.com>
2014-07-18 11:22:43 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,24 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## I hope I did not miss something here, but the code is simple
+## The CacheMatrix function simnply asks for a set of values and the size of the matrix
+## It then caches that matrix into the A variable
+## Cachesolve is a function of a matrix (in this case A)
+## enter in the matrix you want to solve and the function shows the matrix and the inverse of that matrix
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makeCacheMatrix <- function(Data, rows, cols) {
+  A <<- matrix(Data, nrow=rows, ncol=cols)
+  print("Matrix Stored as A")
 }
 
+CacheSolve <- function(mat) {
+  print("Original Matrix")
+  print(mat)
+  print("Inverse of Original Matrix")
+  Invert <<- solve(mat)
+  print(Invert)
+}
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
 }
